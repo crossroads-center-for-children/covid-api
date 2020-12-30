f1e0b067edafd99bd3b7898e4c83505c3249807d
Matt Ramotar <matt.ramotar@jhu.edu>
2020-12-30 15:04:59 -0500

    Add models

---
+++ api/models/index.js
@@ -0,0 +1,19 @@
+const Answer = require("./Answer");
+const Question = require("./Question");
+const Questionnaire = require("./Questionnaire");
+const Response = require("./Response");
+const Room = require("./Room");
+const Student = require("./Student");
+const Tag = require("./Tag");
+const User = require("./User");
+
+module.exports = {
+  Answer,
+  Question,
+  Questionnaire,
+  Response,
+  Room,
+  Student,
+  Tag,
+  User,
+};
