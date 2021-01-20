5f2de9d872fc94d06444d646bcd0e8e04de0f268
Matt Ramotar <matt.ramotar@jhu.edu>
2021-01-19 20:43:01 -0500

    Update response model

--- api/models/Response.js
+++ api/models/Response.js
@@ -1,4 +1,10 @@
 const mongoose = require("mongoose");
+const { sendFailureEmail } = require("../services/email");
+const User = require("./User");
+const Student = require("./Student");
+const Answer = require("./Answer");
+const Question = require("./Question");
+
 mongoose.set("useCreateIndex", true);
 
 const Schema = mongoose.Schema;
@@ -17,6 +23,42 @@ const ResponseSchema = new Schema({
     type: String,
     enum: ["pass", "fail"],
   },
+});
+
+ResponseSchema.pre("save", async function (next) {
+  const response = this;
+
+  if (!response.isModified("status")) {
+    console.log("not modified");
+    return next();
+  }
+
+  if (response.status === "fail") {
+    const user = await User.findById(response.user);
+    const student = await Student.findById(response.student);
+
+    const answers = [];
+
+    for (const answer of response.answers) {
+      answers.push(
+        await Answer.findById(answer).populate({
+          path: "question",
+          model: Question,
+        })
+      );
+    }
+
+    await sendFailureEmail({
+      submitted: response.submitted,
+      date: response.date,
+
+      user,
+      student,
+      answers,
+    });
+  }
+
+  next();
 });
 
 module.exports = mongoose.model("Response", ResponseSchema);
